e9366525ce18b3eabeb473caddc6be9cae80adb8
lzogimenez <lgimenez@fi.uba.ar>
2020-07-10 12:38:40 -0300

    Query a movieDB con actor

--- tdl-akka-project/src/main/scala/com/example/QuickstartApp.scala
+++ tdl-akka-project/src/main/scala/com/example/QuickstartApp.scala
@@ -1,16 +1,42 @@
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
+import akka.actor.{ Actor, ActorRef, ActorSystem, Props }
 import akka.stream.ActorMaterializer
 import akka.http.scaladsl.unmarshalling.Unmarshal
 import scala.concurrent.Await
 import scala.concurrent.duration._
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import spray.json.DefaultJsonProtocol._
-
-
 import scala.concurrent.Future
 import scala.util.{ Failure, Success }
+
+class buscadorPeliculas(system: ActorSystem, materializer: ActorMaterializer) extends Actor {
+
+  def receive = {
+    case query:String => {
+
+      implicit val Actsystem = system;
+      implicit val Actmat = materializer;
+      implicit val executionContext = system.dispatcher
+      val movieQuery = query
+      val queryString = Some("api_key=0f4c286aea338ef131e2ed9b2b522856&language=en-US&page=1&include_adult=false&query=" ++ movieQuery)
+      val movieDBUri = Uri.from(scheme = "http", host = "api.themoviedb.org", path = "/3/search/movie", queryString = queryString)
+      val responseFuture: Future[HttpResponse] = Http().singleRequest(HttpRequest(uri = movieDBUri))
+
+      responseFuture.onComplete {
+        case Success(res) => {
+          println("Llego respuesta de la API para: " ++ query)
+          val stringFuture: Future[String] = Unmarshal(res).to[String]
+          println(Await.result(stringFuture, 5 seconds))
+        }
+        case Failure(_)   => sys.error("Ocurrio un error al esperar la respuesta de la API")
+      }
+    }
+    case _ => println("Buscador recibio una query que no es del tipo string<")
+  }
+
+}
 
 object Client {
   def main(args: Array[String]): Unit = {
@@ -18,24 +44,8 @@ object Client {
     implicit val materializer = ActorMaterializer()
     // needed for the future flatMap/onComplete in the end
     implicit val executionContext = system.dispatcher
- 
-
-    val movieQuery = "enano"
-
-    val queryString = Some("api_key=0f4c286aea338ef131e2ed9b2b522856&language=en-US&page=1&include_adult=false&query=" ++ movieQuery)
-
-    val movieDBUri = Uri.from(scheme = "http", host = "api.themoviedb.org", path = "/3/search/movie", queryString = queryString)
-
-    val responseFuture: Future[HttpResponse] = Http().singleRequest(HttpRequest(uri = movieDBUri))
-
-    responseFuture
-      .onComplete {
-        case Success(res) => {
-            println("Llego respuesta de la API")
-            val stringFuture: Future[String] = Unmarshal(res).to[String]
-            println(Await.result(stringFuture, 5 seconds))
-        }
-        case Failure(_)   => sys.error("something wrong")
-      }
+    val buscador = system.actorOf(Props(classOf[buscadorPeliculas], system, materializer), "buscador")
+    buscador ! "robot"
+    buscador ! "alien"
   }
 }
