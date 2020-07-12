3875681675b9a2b010185d2aa82edf1391b9407e
Tomas Lopez Hidalgo <tomas.lopezhidalgo@outlook.com>
2020-07-12 16:45:55 -0300

    Agrega actor para las recomendaciones

--- tdl-akka-project/src/main/scala/com/example/ClienteMovieDB.scala
+++ tdl-akka-project/src/main/scala/com/example/ClienteMovieDB.scala
@@ -21,11 +21,11 @@ import scala.concurrent.duration._
  *  cases clases en este caso es MovieDBProtocol
  */ 
 
-case class Movie(title: String, vote_average: Int)
+case class Movie(title: String, vote_average: Int, id: Int, overview: String)
 case class Page[Movie](page: Int, results: List[Movie])
 
 object MovieDBProtocol extends DefaultJsonProtocol {
-  implicit def movieFormat = jsonFormat2(Movie)
+  implicit def movieFormat = jsonFormat4(Movie)
   implicit def pageFormat[Movie: JsonFormat] = jsonFormat2(Page.apply[Movie])
 }
 
@@ -41,7 +41,7 @@ class MovieDataFormatter extends Actor {
       println("--------------------------------------")
 
       for(movie <- page.results) {
-        println(s"--- Título: ${movie.title}  |  Puntuación: ${movie.vote_average}")
+        println(s"--- ID: ${movie.id} | Título: ${movie.title} | Puntuación: ${movie.vote_average}")
         println("--------------------------------------")
       }
 
@@ -49,8 +49,11 @@ class MovieDataFormatter extends Actor {
   }
 }
 
+// TODO: Refactorizar a un RequestsDispatcher que maneje los requests a la API, estos actores solo deberían 
+//       setear el endpoint y el método junto con los parámetros
+//
 
-class buscadorPeliculas(system: ActorSystem, var formatter: ActorRef) extends Actor {
+class MovieFinder(system: ActorSystem, val apiKey: String, var formatter: ActorRef) extends Actor {
 
   def receive = {
     case query:String => {
@@ -58,7 +61,7 @@ class buscadorPeliculas(system: ActorSystem, var formatter: ActorRef) extends Ac
       implicit val actSystem = system; // Para el Http()
       implicit val executionContext = system.dispatcher // Para poder manejar Future[t].onComplete
 
-      val queryString = Some(s"api_key=0f4c286aea338ef131e2ed9b2b522856&language=en-US&page=1&include_adult=false&query=${query}")
+      val queryString = Some(s"api_key=${apiKey}&language=es-LA&page=1&include_adult=false&query=${query}")
       val movieDBUri = Uri.from(scheme = "http", host = "api.themoviedb.org", path = "/3/search/movie", queryString = queryString)
       val responseFuture: Future[HttpResponse] = Http().singleRequest(HttpRequest(uri = movieDBUri))
 
@@ -80,15 +83,52 @@ class buscadorPeliculas(system: ActorSystem, var formatter: ActorRef) extends Ac
 
 }
 
+
+class MovieRecommender(system: ActorSystem, val apiKey: String, var formatter: ActorRef) extends Actor {
+
+  def receive = {
+    case movieID:String => {
+
+      implicit val actSystem = system; // Para el Http()
+      implicit val executionContext = system.dispatcher // Para poder manejar Future[t].onComplete
+
+      val path = "/3/movie/%s/recommendations".format(movieID)
+
+      val queryString = Some(s"api_key=${apiKey}&language=es-LA&page=1")
+      val movieDBUri = Uri.from(scheme = "http", host = "api.themoviedb.org", path = path, queryString = queryString)
+      val responseFuture: Future[HttpResponse] = Http().singleRequest(HttpRequest(uri = movieDBUri))
+
+      responseFuture.onComplete {
+
+        case Success(res) => {
+          println(s"Response for endpoint=${path} with code=${res.status.intValue()}")
+
+          val stringFuture: Future[String] = Unmarshal(res).to[String]
+
+          println(s"Recommendations for ID: ${movieID}")
+          formatter ! Await.result(stringFuture, 5 seconds) 
+
+        }
+        case Failure(_)   => sys.error("Ocurrio un error al esperar la respuesta de la API")
+      }
+    }
+    case _ => println("Buscador recibio una query que no es del tipo string<")
+  }
+}
+
+
 object Client {
   def main(args: Array[String]): Unit = {
 
     val system = ActorSystem()
+    val APIKey = "0f4c286aea338ef131e2ed9b2b522856"
 
-    val formatter = system.actorOf(Props[MovieDataFormatter], "formatter")
-    val buscador = system.actorOf(Props(classOf[buscadorPeliculas], system, formatter), "buscador")
+    val dataFormatter = system.actorOf(Props[MovieDataFormatter], "formatter")
+    val movieFinder = system.actorOf(Props(classOf[MovieFinder], system, APIKey, dataFormatter), "finder")
+    val recommender = system.actorOf(Props(classOf[MovieRecommender], system, APIKey, dataFormatter), "recommender")
 
-    buscador ! "robot"
-    buscador ! "alien"
+    movieFinder ! "robot"
+    recommender ! "2048"
+//    movieFinder ! "alien"
   }
 }
