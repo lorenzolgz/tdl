07364dea592288f8c7d6f3ae9c20c4671eb19892
Tomas Lopez Hidalgo <tomas.lopezhidalgo@outlook.com>
2020-07-12 15:17:56 -0300

    Agrega parseo de JSON para obtener campos principales

--- tdl-akka-project/src/main/scala/com/example/ClienteMovieDB.scala
+++ tdl-akka-project/src/main/scala/com/example/ClienteMovieDB.scala
@@ -5,19 +5,46 @@ import akka.stream.ActorMaterializer
 import akka.http.scaladsl.unmarshalling.Unmarshal
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 
-import spray.json._
+import spray.json.{JsValue, JsonFormat, _}
+import DefaultJsonProtocol._
 
 import scala.concurrent.{Future, Await}
 import scala.util.{ Failure, Success }
 import scala.concurrent.duration._
 
+/*  
+ *  Para parsear un JSON en scala la forma más fácil de hacerlo es 
+ *  armar case clases que le peguen a cada campo (no hace falta utilizarlos todos)
+ *  en este caso creamos uno para la página en sí (que contiene el número y una lista
+ *  de películas); y otro para la película (que contiene el título, autor, etc.)
+ *  Además hay que crear un protocolo de unmarshalling para poder utilizar estas 
+ *  cases clases en este caso es MovieDBProtocol
+ */ 
+
+case class Movie(title: String, vote_average: Int)
+case class Page[Movie](page: Int, results: List[Movie])
+
+object MovieDBProtocol extends DefaultJsonProtocol {
+  implicit def movieFormat = jsonFormat2(Movie)
+  implicit def pageFormat[Movie: JsonFormat] = jsonFormat2(Page.apply[Movie])
+}
 
 class MovieDataFormatter extends Actor {
+  import MovieDBProtocol._
 
   def receive = {
     case movieData: String => {
-      val prettyPrintFn = JsonParser(movieData).prettyPrint
-      println(prettyPrintFn)
+
+      var page = movieData.parseJson.convertTo[Page[Movie]]
+
+      println(s"Página: ${page.page}")
+      println("--------------------------------------")
+
+      for(movie <- page.results) {
+        println(s"--- Título: ${movie.title}  |  Puntuación: ${movie.vote_average}")
+        println("--------------------------------------")
+      }
+
     }
   }
 }
@@ -31,14 +58,15 @@ class buscadorPeliculas(system: ActorSystem, var formatter: ActorRef) extends Ac
       implicit val actSystem = system; // Para el Http()
       implicit val executionContext = system.dispatcher // Para poder manejar Future[t].onComplete
 
-      val movieQuery = query
-      val queryString = Some("api_key=0f4c286aea338ef131e2ed9b2b522856&language=en-US&page=1&include_adult=false&query=" ++ movieQuery)
+      val queryString = Some(s"api_key=0f4c286aea338ef131e2ed9b2b522856&language=en-US&page=1&include_adult=false&query=${query}")
       val movieDBUri = Uri.from(scheme = "http", host = "api.themoviedb.org", path = "/3/search/movie", queryString = queryString)
       val responseFuture: Future[HttpResponse] = Http().singleRequest(HttpRequest(uri = movieDBUri))
 
       responseFuture.onComplete {
+
         case Success(res) => {
-          println("Llego respuesta de la API para: " ++ query)
+          println(s"Response for key=${query} with code=${res.status.intValue()}")
+
           val stringFuture: Future[String] = Unmarshal(res).to[String]
 
           formatter ! Await.result(stringFuture, 5 seconds) 
